ffda1ba2972ca73cfdd87a4f58f6ef54f2a850bc
ryanolasco.jdk <ryanolasco@gmail.com>
2020-09-26 15:04:13 +0800

    attachment

--- resources/views/admin/sales/payment-details.blade.php
+++ resources/views/admin/sales/payment-details.blade.php
@@ -4,7 +4,7 @@
     <td class="pd-t-20">{{$payment->receipt_number}}</td>
     <td>{{$payment->payment_date}}</td>
     <td>{{$payment->payment_type}}</td>
-    <td><a target="_blank" href="{{ asset('storage/payments/'.$payment->sales_header_id.'/'.$payment->attachment) }}">View</a></td>
+    <td><a target="_blank" href="{{ asset('storage/payments/'.$payment->id.'/'.$payment->attachment) }}">View</a></td>
     <td class="text-right">{{number_format($payment->amount,2)}}</td>
     <td>{{$payment->status}}</td>
     <td>
