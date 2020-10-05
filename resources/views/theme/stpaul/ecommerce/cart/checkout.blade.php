d0f1aa334e5585b9c13fcf09809287ff897d54f2
wsiphproduction <wsiphproduction@gmail.com>
2020-10-05 17:10:22 +0800

    checkout.blade.php updated

--- resources/views/theme/stpaul/ecommerce/cart/checkout.blade.php
+++ resources/views/theme/stpaul/ecommerce/cart/checkout.blade.php
@@ -501,52 +501,6 @@
                 return true;
 
             });
-
-            /** Custom Input number increment js **/
-            jQuery(".quantity").each(function() {
-                var spinner = jQuery(this),
-                    input = spinner.find('input[type="number"]'),
-                    btnUp = spinner.find(".quantity-up"),
-                    btnDown = spinner.find(".quantity-down"),
-                    min = input.attr("min"),
-                    max = input.attr("max"),
-                    valOfAmout = input.val(),
-                    newVal = 0;
-
-                btnUp.on("click", function() {
-                    var varholder = input.val();
-                    var oldValue = parseFloat(input.val());
-
-                    if (varholder === "") {
-                        var newVal = 1;
-                    } else {
-                        if (oldValue >= max) {
-                            var newVal = oldValue;
-                        } else {
-                            var newVal = oldValue + 1;
-                        }
-                    }
-                    spinner.find("input").val(newVal);
-                    spinner.find("input").trigger("change");
-                });
-
-                btnDown.on("click", function() {
-                    var varholder = input.val();
-                    var oldValue = parseFloat(input.val());
-
-                    if (varholder === "") {
-                        var newVal = 1;
-                    } else {
-                        if (oldValue <= min) {
-                            var newVal = oldValue;
-                        } else {
-                            var newVal = oldValue - 1;
-                        }
-                    }
-                    spinner.find("input").val(newVal);
-                    spinner.find("input").trigger("change");
-                });
-            });
         });
 
         $(document).ready(function() {
