4ee9d261aa91f191d3d6ce98ddc64ebaca6a7375
ryanolasco.jdk <ryanolasco@gmail.com>
2020-10-07 14:05:34 +0800

    message

--- resources/views/admin/shippingfee/index.blade.php
+++ resources/views/admin/shippingfee/index.blade.php
@@ -203,7 +203,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>Are you sure you want to delete this rate?</p>
+                    <p>Deleted items in this module cannot be restored. Are you sure you want to delete this rate?</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-sm btn-danger" id="btnDelete">Yes, Delete</button> 
