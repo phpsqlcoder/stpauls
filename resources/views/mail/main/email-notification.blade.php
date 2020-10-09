0ad534d67dd9c34a1d60c322c9bc388479103005
ryanolasco.jdk <ryanolasco@gmail.com>
2020-10-09 09:52:25 +0800

    email notif

--- resources/views/mail/main/email-notification.blade.php
+++ resources/views/mail/main/email-notification.blade.php
@@ -119,8 +119,8 @@
 
                         @foreach($sales->items as $item)
                         @php
-                            $weight += $item->product->weight*$item->qty;
-                            $totalamount += $item->price*$item->qty;
+                            $weight = $item->product->weight*$item->qty;
+                            $totalamount = $item->price*$item->qty;
                             $subtotal += $totalamount;
                             $totalweight += $weight;
                         @endphp
