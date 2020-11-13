a0ab70fa69f79726cc267378b47158b4eee868bf
ryanolasco.jdk <ryanolasco@gmail.com>
2020-11-13 11:22:21 +0800

    cors

--- resources/views/theme/stpaul/main.blade.php
+++ resources/views/theme/stpaul/main.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
+    <meta http-equiv="Access-Control-Allow-Origin" content="*">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>ST PAULS Online | Catholic Online Bookstore in Philippines</title>
