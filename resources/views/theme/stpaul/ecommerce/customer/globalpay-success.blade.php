0f2c1d4a846f341b4522721bb2d73d18e997cb77
ryanolasco.jdk <ryanolasco@gmail.com>
2020-11-24 14:48:07 +0800

    success order

--- resources/views/theme/stpaul/ecommerce/customer/globalpay-success.blade.php
+++ resources/views/theme/stpaul/ecommerce/customer/globalpay-success.blade.php
@@ -51,7 +51,7 @@
                                     @if($sales->sdd_booking_type == 1 && $payment > 0)
                                     <p class="text-bold"><b>To book a rider, click on the &nbsp;<span class="lnr lnr-bicycle mr-2"></span> and enter the Rider's Information.</b></p>
                                     @else
-                                    <p class="text-bold"><b>To view the status of your order, please click &nbsp;<span class="lnr lnr-eye text-md text-first-color font-weight-bold"></span>&nbsp;or&nbsp;<span class="lnr lnr-car text-md text-first-color font-weight-bold mr-2"></span>.</b></p>
+                                    <p class="text-bold">For <b class="text-danger">unpaid orders</b>, click on <b class="text-danger">PAY NOW</b> button to attach your proof of payment.<br><b>To view the status of your order, please click &nbsp;<span class="lnr lnr-eye text-md text-first-color font-weight-bold"></span>&nbsp;or&nbsp;<span class="lnr lnr-car text-md text-first-color font-weight-bold mr-2"></span>.</b></p>
                                     @endif
                                 </div>
                                 <div class="col-md-5 mb-4 align-self-center">
