7cf66e51403493b955bafce94990cd54258462f4
darrenbawag <renash12@gmail.com>
2020-11-27 12:49:14 +0800

    Change direction globaypay success message to vertical

--- resources/views/theme/stpaul/ecommerce/customer/globalpay-success.blade.php
+++ resources/views/theme/stpaul/ecommerce/customer/globalpay-success.blade.php
@@ -48,14 +48,14 @@
                                     </div>
                                     <div class="gap-40"></div>
                                 </div>
-                                <div class="col-md-7 mb-4">
+                                <div class="col-md-12 mb-4">
                                     @if($sales->sdd_booking_type == 1 && $payment > 0)
                                     <p class="font-weight-bold"><b>To book a rider, click on the &nbsp;<span class="lnr lnr-bicycle text-md text-first-color font-weight-bold mr-2"></span> and enter the Rider's Information.</b></p>
                                     @else
                                     <p class="font-weight-bold">For <b class="text-danger">unpaid orders</b>, click on <b class="text-danger">MY ORDER</b> button below. Click on the <span class="d-inline-block ml-1 mr-1" style="transform:translateY(6px)"><span class="c-icon c-icon-peso-red"></span></span> button to attach your proof of payment. <b>To view the status of your order, click on &nbsp;<span class="d-inline-block mr-1" style="transform:translateY(6.5px)"><span class="lnr lnr-eye text-md text-first-color font-weight-bold"></span></span>&nbsp;or&nbsp;<span class="d-inline-block ml-1" style="transform:translateY(2px)"><span class="lnr lnr-car text-md text-first-color font-weight-bold mr-2"></span></span>.</b></p>
                                     @endif
                                 </div>
-                                <div class="col-md-5 mb-4">
+                                <div class="col-md-12 mb-4">
                                     <a href="{{ route('account-my-orders') }}" class="btn btn-md primary-btn btn-block text-white">My Order</a>
                                 </div>
                                 <div class="col-lg-12">
