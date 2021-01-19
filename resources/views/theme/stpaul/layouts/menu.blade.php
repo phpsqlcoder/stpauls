4037ac1fc189e505daa38a15f3bf7a1c0efde3cb
ryanolasco.jdk <ryanolasco@gmail.com>
2021-01-19 16:38:30 +0800

    menu

--- resources/views/theme/stpaul/layouts/menu.blade.php
+++ resources/views/theme/stpaul/layouts/menu.blade.php
@@ -10,7 +10,7 @@
         <a href="{{ route('front.branches') }}">Branches</a>
     </li>
 
-    <li @if (\Route::current()->getName() == 'front.branches') class="active" @endif>
+    <li @if (\Route::current()->getName() == 'front.request-a-title') class="active" @endif>
         <a href="{{ route('front.request-a-title') }}">REQUEST A TITLE</a>
     </li>
 </ul>
